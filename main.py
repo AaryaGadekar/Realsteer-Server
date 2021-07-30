a767d3b14df756c0b2b77c8106a525606ed90aec
Aarya Gadekar <aaryagadekar01@gmail.com>
2021-07-30 08:18:18 +0530

    CLI interface update

--- main.py
+++ main.py
@@ -1,50 +1,107 @@
 import socket
 import vgamepad as vg
+import argparse
+
+
+class NetworkInfo:
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)
+    UDP_IP = str(socket.gethostbyname(socket.gethostname()))
+    udp_port = 0
+    didReceiveData = False
+    sens = 0.6
+    running = False
+
+    def __init__(self, port=None) -> None:
+        if(port != None):
+            self.udp_port = port
+            self.didReceiveData = False
+            self.sens = 0.6
+            self.running = False
+
+    def storePort(self, portInt):
+        try:
+            self.udp_port = portInt
+            self.__init__
+        except:
+            raise TypeError("Expected integer as port number")
+
+    def storeSens(self, sensInt):
+        try:
+            self.sens = sensInt
+        except:
+            raise TypeError("Expected integer as sensitivity")
+
+    def processData(self):
+        self.sock.bind((self.UDP_IP, self.udp_port))
+        while True:
+            try:
+                data, addr = net.sock.recvfrom(
+                    1024)  # buffer size is 1024 bytes
+                data = data.decode('UTF-8')
+                data = data[1:-1].split(',')
+                # didReceiveData = True
+
+                while (data == None):
+                    steering = 0
+                    accelerator = 'false'
+                    brake = 'false'
+                    gamepad.right_trigger(value=0)
+                    gamepad.left_trigger(value=0)
+                    gamepad.left_joystick_float(
+                        x_value_float=0, y_value_float=0)
+                    gamepad.update()
+                    print(steering)
+                    print(accelerator)
+                    print(brake)
+
+                steering = (round((float(data[0][1:-1])), 4))
+                if (steering > net.sens):
+                    steering = net.sens
+                elif (steering < -net.sens):
+                    steering = -net.sens
+                accelerator = ((data[1][1:-1]))
+                brake = ((data[2][1:-1]))
+
+                if(accelerator == 'true'):
+                    gamepad.right_trigger(value=255)
+                else:
+                    gamepad.right_trigger(value=0)
+                if(brake == 'true'):
+                    gamepad.left_trigger(value=255)
+                else:
+                    gamepad.left_trigger(value=0)
+
+                print(f"Accelerator:{accelerator}")
+                print(f"Brake:{brake}")
+                print(f"Steering:{getNewValue(steering)}")
+                gamepad.left_joystick_float(x_value_float=getNewValue(
+                    steering), y_value_float=0)  # values between -1.0 and 1.0
+                gamepad.update()
+            except KeyboardInterrupt:
+                break
+
 
 gamepad = vg.VX360Gamepad()
+net = NetworkInfo()
 
-UDP_IP = "192.168.0.152"
-UDP_PORT = 50000
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
 
 def getNewValue(n):
-    oldRange = (0.6+0.6) #oldMax -Oldmin
-    newRange = (1+1) #newMax - newMin
-    newValue = (((n + 0.6)*newRange)/oldRange) - 1
+    oldRange = (net.sens+net.sens)  # oldMax -Oldmin
+    newRange = (1+1)  # newMax - newMin
+    newValue = (((n + net.sens)*newRange)/oldRange) - 1
     return newValue
 
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    data = data.decode('UTF-8')
-    data = data[1:-1].split(',')
+def main():
+    my_parser = argparse.ArgumentParser()
+    my_parser.version = '1.0'
+    my_parser.add_argument('port', default=50000, const=50000, nargs='?')
+    my_parser.add_argument('sensitivity', default=0.6, const=0.6, nargs='?')
+    args = my_parser.parse_args()
+    net.storePort(int(args.port))
+    net.processData()
 
-    if (data==None):
-        steering = 0
-        accelerator = 'false'
-        brake = 'false'
-    else:
-        steering = (round((float(data[0][1:-1])),4))
-        if (steering > 0.6):
-            steering = 0.6
-        elif (steering < -0.6):
-            steering = -0.6
-        accelerator = ((data[1][1:-1]))
-        brake = ((data[2][1:-1]))
-    if(accelerator == 'true'):
-        gamepad.right_trigger(value=255)
-    else:
-        gamepad.right_trigger(value=0)
-    if(brake == 'true'):
-        gamepad.left_trigger(value=255)
-    else:
-        gamepad.left_trigger(value=0)
-    
-    print(f"Accelerator:{accelerator}")
-    print(f"Brake:{brake}")
-    print(f"Steering:{getNewValue(steering)}")
-    gamepad.left_joystick_float(x_value_float=getNewValue(steering), y_value_float=0)  # values between -1.0 and 1.0
-    gamepad.update()
+
+if __name__ == '__main__':
+    main()
